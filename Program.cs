8a66584987abe2af5e9766a0fd4b6f00a7e69cf8
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2022-05-06 10:04:02 -0400

    list inside row vs col layout updates

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using IgniteUI.Blazor.Controls;
-using TestingComponentsDontDelete;
+using ListInsideRowVsColLayout;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -16,28 +16,8 @@ await builder.Build().RunAsync();
 void RegisterIgniteUI(IServiceCollection services)
 {
     services.AddIgniteUIBlazor(
-        typeof(IgbNavbarModule),
-        typeof(IgbButtonModule),
-        typeof(IgbIconModule),
-        typeof(IgbRippleModule),
-        typeof(IgbNavDrawerModule),
-        typeof(IgbNavDrawerItemModule),
-        typeof(IgbCardModule),
-        typeof(IgbAvatarModule),
         typeof(IgbListModule),
-        typeof(IgbDataGridModule),
-        typeof(IgbDataGridToolbarModule),
-        typeof(IgbInputModule),
-        typeof(IgbDatePickerModule),
-        typeof(IgbSliderModule),
-        typeof(IgbChipModule),
-        typeof(IgbRangeSliderModule),
-        typeof(IgbSwitchModule),
-        typeof(IgbCheckboxModule),
-        typeof(IgbRadioGroupModule),
-        typeof(IgbRadioModule),
-        typeof(IgbDropdownModule),
-        typeof(IgbDropdownItemModule),
-        typeof(IgbSnackbarModule)
+        typeof(IgbAvatarModule),
+        typeof(IgbIconModule)
     );
 }
