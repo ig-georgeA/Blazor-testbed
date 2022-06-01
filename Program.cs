22bd534828d8f01a3b476b339025a41e351d0e28
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2022-06-01 08:45:51 -0400

    Blazor Testbed updates

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using IgniteUI.Blazor.Controls;
 using BlazorTestbed;
+using BlazorTestbed.Northwind;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +10,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<NorthwindService>();
+
 RegisterIgniteUI(builder.Services);
 
 await builder.Build().RunAsync();
